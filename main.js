05e7dd84e2c1f97f16c1b136860a9bedabd91a83
@racascou <rafaelcastrocouto@gmail.com>
2025-09-03 15:54:03 +0000

    modified:   main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ window.invertMode = false;
 window.invertBack = false;
 window.anim = false;
 
-import {default as midi, askMidiPermissions, pressed} from 'midi';
-import {default as audioInit, audioLoop, popInit} from 'audio';
+import {default as midi, askMidiPermissions, pressed} from '/midi';
+import {default as audioInit, audioLoop, popInit} from '/audio';
 
 let iCtrl1,iCtrl2,iCtrl3,iCtrl4,iCtrl5,iCtrl6,iCtrl7,iCtrl8;
 
